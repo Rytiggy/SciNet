33f8901f7ba120c2b7de639040746bfb42b23126
Ryan Mason <rmm4514@rit.edu>
2017-02-01 12:05:52 -0500

    Merge Cam's and Mine together

--- extension/content.js
+++ extension/content.js
@@ -1,3 +1,22 @@
+// content.js
+
+
+// logs URL of the first external link on the page
 var firstHref = $("a[href^='http']").eq(0).attr("href");
 
 console.log(firstHref);
+
+chrome.runtime.sendMessage({greeting: "hello"}, function(response) {
+  console.log(response.farewell);
+  console.log(response.url);
+  if (response.url.includes("google")) {
+    // look for citations to parse :~)
+    console.log("looking for citations...");
+  }
+});
+
+// Okay, so what's required here?
+// Specifications:
+// 1. The extension recognizes when it's on a publisher's page
+// 2. The extension, when on a page with citations, scrapes those citations.
+// 3. The extension takes those scraped citations and sends them to a backend server.
