46c3669ebdb3ad3e885a51d84e428a20c7b199fe
Ryan Mason <rmm4514@rit.edu>
2017-02-06 12:39:46 -0500

    Search any page for any keywords and display any blocks of text that have those keywords in it.

--- extension/assets/js/background.js
+++ extension/assets/js/background.js
@@ -1,10 +1,13 @@
 chrome.runtime.onMessage.addListener(
-  function(request, sender, sendResponse) {
-    console.log(sender.tab ?
-                "from a content script: " + sender.tab.url :
-                "from the extension");
 
-    if (request.greeting == "hello") {
-      sendResponse({farewell: "goodbye", url: sender.tab.url});
-    }
-  });
+  // function(request, sender, sendResponse) {
+
+  //   console.log(sender.tab ?
+  //               "from a content script: " + sender.tab.url :
+  //               "from the extension");
+
+  //   if (request.greeting == "hello") {
+  //     sendResponse({farewell: "goodbye", url: sender.tab.url});
+  //   }
+  // }
+  );
--- extension/assets/js/content.js
+++ extension/assets/js/content.js
@@ -4,19 +4,34 @@
 // logs URL of the first external link on the page
 var firstHref = $("a[href^='http']").eq(0).attr("href");
 
-console.log(firstHref);
-
 chrome.runtime.sendMessage({greeting: "hello"}, function(response) {
-  console.log(response.farewell);
-  console.log(response.url);
-  if (response.url.includes("google")) {
-    // look for citations to parse :~)
-    console.log("looking for citations...");
-  }
+  // console.log(response.farewell);
+  // console.log(response.url);
+  	
+	//find all blocks of text containing the word abstract   	    
+  	 var foundin = $('div:contains("Abstract")');
+  	 foundin.each(function(){
+        console.log($(this).text());
+     });
+
+
+
+
+
+
+
+  // if (response.url.includes("google")) {
+  //   // look for citations to parse :~)
+  //   console.log("looking for citations...");
+  // }
 });
+
 
 // Okay, so what's required here?
 // Specifications:
 // 1. The extension recognizes when it's on a publisher's page
 // 2. The extension, when on a page with citations, scrapes those citations.
+	//gets the entire body
+	//var html = document.body.innerHTML;
+
 // 3. The extension takes those scraped citations and sends them to a backend server.
