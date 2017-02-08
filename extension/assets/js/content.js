e522ff8ee24d247c856902773433c4742f764d81
Ryan Mason <rmm4514@rit.edu>
2017-02-08 11:28:01 -0500

    Checks page that you are on before running the scraper

--- extension/assets/js/content.js
+++ extension/assets/js/content.js
@@ -7,12 +7,39 @@ var firstHref = $("a[href^='http']").eq(0).attr("href");
 chrome.runtime.sendMessage({greeting: "hello"}, function(response) {
   // console.log(response.farewell);
   // console.log(response.url);
-  	
-	//find all blocks of text containing the word abstract   	    
-  	 var foundin = $('div:contains("Abstract")');
-  	 foundin.each(function(){
-        console.log($(this).text());
-     });
+
+  	//JSON array of acceptable sites to parse
+  	//http://science.sciencemag.org/
+  	//document.location.hostname
+  	obj = [
+	  	{
+			"site": "science.sciencemag.org"
+		}, 
+		{
+			"site": "ryanmasonline.com"
+		}
+	]
+
+for (var i = 0; i <= obj.length; i++)
+{
+	try{
+      var site = obj[i].site;
+      if(document.location.hostname == site){
+        console.log(site);	
+
+		//find all blocks of text containing the word abstract   	    
+	  	 var foundin = $('div:contains("Abstract")');
+	  	 foundin.each(function(){
+	        console.log($(this).text());
+	     });
+      }
+  	}catch(err){
+  	  console.log("Error code 0 dont worry about this");
+
+  	}
+
+}
+
 
 
 
@@ -30,6 +57,7 @@ chrome.runtime.sendMessage({greeting: "hello"}, function(response) {
 // Okay, so what's required here?
 // Specifications:
 // 1. The extension recognizes when it's on a publisher's page
+	 //what makes a publisher's page a publisher's page
 // 2. The extension, when on a page with citations, scrapes those citations.
 	//gets the entire body
 	//var html = document.body.innerHTML;
